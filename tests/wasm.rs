f12b63a0b4bdf19a1bb951711a4115a0dee05d27
Daniel Gallups <dsgallups@protonmail.com>
2024-11-03 11:27:54 -0500

    fix: wasm-pack config

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -1,5 +1,8 @@
 use wasm_bindgen_test::*;
 //use wasm_tracing::prelude::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
 #[wasm_bindgen_test]
 pub fn test() {
     wasm_tracing::set_as_global_default();
